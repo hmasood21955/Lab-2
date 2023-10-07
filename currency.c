14e1909c9d1526fe6e4a948f04686335bdbd2d21
hmasood21955 <hmasood878@gmail.com>
2023-10-07 22:49:09 +0500

    program currency exchange

---
+++ currency.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    float USD;
+    printf("enter the value of USD FOR EXCHAGE");
+    scanf("%f",&USD);
+    int pk =279;
+    USD=USD*pk;
+    printf("USD to PK IS %f",USD);
+    
+}
