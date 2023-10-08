b2b64a98780eb94c3f3a6c2a4624741f67bd93f8
hmasood21955 <hmasood878@gmail.com>
2023-10-08 23:38:54 +0500

    correction

--- mixed_2.c
+++ mixed_2.c
@@ -1,7 +1,9 @@
 #include<stdio.h>
 int main()
 {
-    int a = 10, b = 20, c;
-    c = a / b;
-    printf("%d",c);
+    int a = 10;
+    float b = 20;
+    float c;
+    c =(float) a / b;
+    printf("%f",c);
 }
