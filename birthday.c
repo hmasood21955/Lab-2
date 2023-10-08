dd3c26ba681933dda6d5dfa1cb7640fefb97a141
hmasood21955 <hmasood878@gmail.com>
2023-10-08 23:41:57 +0500

    Update birthday.c

--- birthday.c
+++ birthday.c
@@ -49,7 +49,7 @@ int main(int argc, char **argv) {
   time_t bdayT = mktime(&bday);
   long diff = (long) difftime(now, bdayT);
 
-  int years = diff / (365* 24 * 60 * 60);+
+  int years = diff / (365* 24 * 60 * 60);
   diff -= years * (365 * 24 * 60 * 60);
   int days = diff / (24 * 60 * 60);
 
