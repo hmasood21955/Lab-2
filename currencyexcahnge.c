6e7edccb1cf0f3ce32ed9536cc3b9083da98fa89
hmasood21955 <hmasood878@gmail.com>
2023-10-08 23:11:48 +0500

    program by understanding statement

---
+++ currencyexcahnge.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main(void)
+{
+    int totalAmount,yens;
+    float charges,remainingAmount,pounds;
+
+    const float YEN_PER_USD = 127.65f;
+    const float POUNDS_PER_USD = 0.79f;
+    printf("Please provide the amount to be converted :");
+    scanf("%d",&totalAmount);
+
+    charges  = (10/100)* totalAmount;
+    remainingAmount = totalAmount - charges;
+
+    yens = (remainingAmount/2) * YEN_PER_USD;
+    pounds = (remainingAmount/2) * POUNDS_PER_USD;
+
+    printf("Your go %f pounds \n  %d yens \n", pounds,yens);
+
+    return 0;
+}
